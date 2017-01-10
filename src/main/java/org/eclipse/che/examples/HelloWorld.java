27e85c10940b7e8ae56a9206bd9a76cc0f085c99
Nikita <work.samp01@gmail.com>
2017-01-10 10:09:12 +0000

    added during Demo

--- src/main/java/org/eclipse/che/examples/HelloWorld.java
+++ src/main/java/org/eclipse/che/examples/HelloWorld.java
@@ -3,6 +3,7 @@ package org.eclipse.che.examples;
 public class HelloWorld {
     public static void main(String[] argvs) {
         String a = "Che";
+        System.out.println("Added during Demo");
         System.out.println("Hello World " + a + "!");
     }
 }
